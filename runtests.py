c4d30ccecc4d49eeeac8b37db850800910104e5a
Alex Parinov <creafz@gmail.com>
2014-10-11 21:21:53 +0400

    Added runtests.py

---
+++ runtests.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+import sys
+import os
+from django.conf import settings
+from django.core.management import call_command
+
+sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
+
+# Added to support Django >= 1.7
+
+settings.configure(
+    INSTALLED_APPS=('social_widgets',),
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': ':MEMORY:',
+        }
+    }
+)
+
+import django
+if django.VERSION[:2] >= (1, 7):
+    django.setup()
+
+if __name__ == "__main__":
+    call_command('test', 'social_widgets')
