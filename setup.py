caf6b751a27e09c8745443d10bec9c91aa5db374
Thomas Leese <inbox@thomasleese.me>
2016-01-11 09:10:21 +0000

    Remove 'encoding' parameter on 'open' call
    It's not available in both Python 2.6 and 2.7 which should be supported.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 from setuptools import setup
 
-with open(os.path.join(os.path.dirname(__file__), 'README.rst'), encoding='utf-8') as readme:
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
     README = readme.read()
 
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
