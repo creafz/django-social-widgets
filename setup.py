5e3f2bfe25b5f66e65e95ed83781051dd4d6a8a6
Michael Pfister <m.pfister@t-systems.com>
2017-10-17 13:47:48 +0200

    removed virtualenv; removed old django and python versions from tox.ini

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-social-widgets',
-    version='0.4.0',
+    version='0.5.0',
     packages=['social_widgets'],
     include_package_data=True,
     license='MIT License',
@@ -19,7 +19,7 @@ setup(
     keywords='Django, social network, template, facebook, twitter',
     url='https://github.com/creafz/django-social-widgets',
     download_url=
-    'https://github.com/creafz/django-social-widgets/tarball/0.4.0',
+    'https://github.com/creafz/django-social-widgets/tarball/0.5.0',
     author='Alex Parinov',
     author_email='creafz@gmail.com',
     classifiers=[
@@ -41,5 +41,5 @@ setup(
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
-    install_requires=["django>=1.5"],
+    install_requires=["django>=1.10"],
 )
