c7e8d1bcf7a44c68105873e45bd42e5f42221eaa
Bhute <omkarbhute26@gmail.com>
2021-08-27 08:35:09 +0530

    tried to solve error
    tried to solve this error: Cannot import 'Context' from 'django.template.base' . by removing 'Context' which is not used anywhere in the code.

--- social_widgets/templatetags/social_widgets.py
+++ social_widgets/templatetags/social_widgets.py
@@ -3,7 +3,7 @@ import re
 from locale import normalize
 
 from django.template import loader, TemplateDoesNotExist
-from django.template.base import Node, Context, TemplateSyntaxError
+from django.template.base import Node, TemplateSyntaxError
 from django.utils.encoding import smart_text
 from django.utils.translation import get_language, to_locale
 
